7a6244431b7f319df032b4109cb9eea410a44929
Sufranata <anggasufranata@gmail.com>
2025-04-25 20:03:34 +0700

    Add files via upload

---
+++ tugas angga2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"masukan kata = HALO"<<endl;
+	cin>>kata;
+	
+	if (kata=="HALO"){
+		cout<<"kata yang di masukan sesuai"<<endl;
+	}else{
+		cout<<"kata yang di masukan tidak sesuai"<<endl;
+	}
+}
+	
